42e7c3464804ee639da6dfcd66b82ee7ae8c8c3b
Martijn <martijn.tielemans@live.nl>
2021-02-04 14:58:57 +0100

    Added a bunch of scripts
    - Added playermovement
    - Added pickup
    - Added IInteractable
    - Added a plane and a sphere to the scene

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    InventoryScript inventory;
+
+    [SerializeField]
+    int initialMaxWeight = 120;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = new InventoryScript(initialMaxWeight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float originalSpeed = 6;
+    public float runSpeed = 9;
+    float currentSpeed;
+    public float jumpHeight = 5;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Run") > 0)
+        {
+            currentSpeed = runSpeed;
+        }
+        else
+        {
+            currentSpeed = originalSpeed;
+        }
+
+        transform.position += new Vector3((Input.GetAxis("Horizontal") * currentSpeed) * Time.deltaTime, 0, ((Input.GetAxis("Vertical") * currentSpeed) * Time.deltaTime));
+    }
+}
