b959aee015b3100456aeb0c1447920638b5fe87f
Martijn <martijn.tielemans@live.nl>
2021-03-04 11:27:23 +0100

    Added doors
    - Added a door model
    - Added a door animation

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -7,12 +7,14 @@ public class DoorScript : MonoBehaviour, IInteractable
     public int doorID = 0;
 
     Animator anim;
+    BoxCollider triggerBox;
 
     // Start is called before the first frame update
     void Start()
     {
         gameObject.tag = "Interactable";
         anim = GetComponent<Animator>();
+        triggerBox = GetComponent<BoxCollider>();
     }
 
     public void Action(PlayerManager player)
@@ -20,10 +22,13 @@ public class DoorScript : MonoBehaviour, IInteractable
         if (player.GetInventory().CanOpenDoor(doorID))
         {
             anim.SetBool("Open", true);
+            triggerBox.enabled = false;
         }
         else
         {
             Debug.Log("Cannot open this door, it's ID is: " + doorID);
+
+            // TODO: Add a little sound to indicate the door is not opening
         }
     }
 }
