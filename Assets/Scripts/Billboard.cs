8a87749065c7a40fddc23e2f54d6022976be8867
Martijn <martijn.tielemans@live.nl>
2021-03-24 13:31:12 +0100

    Finalised Terrain
    - Finalised the terrain layout
    - Added a Palm Tree
    - Added a pier to the island

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookAtPos = player.transform.position;
+        lookAtPos.y = transform.position.y;
+
+        // Has the object constantly look at the player's camera
+        transform.LookAt(lookAtPos, Vector3.up);
+    }
+}
